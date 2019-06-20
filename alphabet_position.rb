cf24904861900c7cedc01a884ff04bc717f07cb1
Charles Kettell <charleskettell@hotmail.co.uk>
2019-06-20 15:14:32 +0000

    letters replaced by their index

---
+++ alphabet_position.rb
@@ -0,0 +1,28 @@
+def alphabet_index
+combined = {}
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+arrange = alphabet.split("").sort
+arrange.each_with_index do |val, index|
+  combined[val] = (index + 1)
+end
+combined
+end
+
+def letters_only(string)
+stripped = string.gsub!(/[^a-zA-Z]/, '')
+stripped
+end
+
+
+
+def alphabet_position(statement)
+  complete = []
+  letters_only(statement)
+  array = statement.downcase.split("")
+  array.map do |letter|
+  complete<< alphabet_index[letter]
+  end
+  p complete.join(" ").to_s
+end
+
+alphabet_position("The sunset sets at twelve o' clock.")
